2733333bf477098bbdd16fa628753bb84e62cf88
Fran Guijarro <franleplant@gmail.com>
2015-07-23 16:51:21 -0300

    add unit tests

--- test.js
+++ test.js
@@ -1,12 +1,46 @@
+import request from 'superagent';
+import sinon from 'sinon';
+import {expect} from 'chai';
 import webtask from './index.js';
-import chalk from 'chalk';
 
 
-let runner = webtask('wt-franleplant-gmail_com-0');
+const HOST_MOCK = 'rules.auth0.com';
+const CONTAINER_MOCK = 'fake-container';
+const WEBTASK_MOCK = 'hello';
+const RESPONSE_MOCK = {ok: true, body: 'hello'};
 
-runner('hello')
-    .then((res) => {
-        console.log(chalk.green('Success!!!'));
-        console.log(chalk.blue('response: '), res)
-    })
-    .catch(console.log.bind(console))
+describe('Webtask Runner', function() {
+    before(function(done){
+        sinon
+            .stub(request, 'get', function getStub(url) {
+                return {
+                    end: cb => cb(null, RESPONSE_MOCK)
+                }
+            })
+        done();
+    });
+
+    after(function(done){
+        request.get.restore();
+        done();
+    });
+
+    it('makes the request to the correct url and return the right value', function(done){
+        let runner = webtask(CONTAINER_MOCK, HOST_MOCK);
+        runner(WEBTASK_MOCK)
+            .catch((err) => {
+                console.log(`the mocked https request failed with err: ${err}`);
+                done();
+            })
+            .then((res) => {
+                expect(request.get.getCall(0).args[0]).to.equal(`https://${HOST_MOCK}/api/run/${CONTAINER_MOCK}/${WEBTASK_MOCK}`);
+                expect(res).to.equal(RESPONSE_MOCK.body);
+                done();
+            })
+            .catch((err) => {
+                console.log('Something went wrong', err);
+            });
+    });
+});
+
+
